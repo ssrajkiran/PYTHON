e6847f0903398aec9985cce6a8febd01e372806c
ssrajkiran <ssrajkiran1@gmail.com>
2021-04-28 07:36:30 +0530

    greatestnumber.py

---
+++ basic/greatestnumber.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if a==b:
+    print("same number")
+elif a>b:
+    print("A is greater: number :{}".format(a))
+else:
+    print("B is greater: number :{}".format(b))
